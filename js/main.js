17ecb3b8e813b2961982cf4f8663261aba60c543
Nbhaskell <nbhaskell@gmail.com>
2016-06-13 22:49:59 -0700

    completed skills section and scroll js animation / cleaned up js animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-// Init Skrollr
-var s = skrollr.init({
-    forceHeight: false
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 2100) {
+  $('.skillbar').each(function() {
+    $(this).find('.skillbar-bar').animate({
+      width: $(this).attr('data-percent')
+    }, 3000);
+  });
+    }
 });
 
-// Refresh Skrollr after resizing our sections
-s.refresh($('.homeSlide'));
+/*$(document).ready(function() {
+  $('.skillbar').each(function() {
+    $(this).find('.skillbar-bar').animate({
+      width: $(this).attr('data-percent')
+    }, 6000);
+  });
+});*/
+
+
+
