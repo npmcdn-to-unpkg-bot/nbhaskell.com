8d0cf0f27d22e660152e1207617f356344f990d5
Nbhaskell <nbhaskell@gmail.com>
2016-06-15 15:09:41 -0700

    still tinkering with responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 2100) {
-  $('.skillbar').each(function() {
-    $(this).find('.skillbar-bar').animate({
-      width: $(this).attr('data-percent')
-    }, 3000);
-  });
-    }
+	 if ($(this).scrollTop() > 2100) {
+		 $('.skillbar').each(function() {
+	   	  $(this).find('.skillbar-bar').animate({
+	      width: $(this).attr('data-percent')
+	    }, 3000);
+    });
+   }
 });
 
-/*$(document).ready(function() {
-  $('.skillbar').each(function() {
-    $(this).find('.skillbar-bar').animate({
-      width: $(this).attr('data-percent')
-    }, 6000);
-  });
-});*/
 
 
 
